91ff632a892e7b9c8fa67163f83b38b53bd6d294
mohamedEmad55 <52850365+mohamedEmad55@users.noreply.github.com>
2022-04-19 13:21:20 +0200

    image-processing-api

---
+++ src/controllers/image.controller.ts
@@ -0,0 +1,25 @@
+import sharp from 'sharp';
+import { Request, Response } from 'express';
+import { fullPath, thumbPath } from '../paths';
+
+const imageController = async (req: Request, res: Response): Promise<void> => {
+  const { filename, width, height } = req.query;
+
+  sharp(fullPath(String(filename)))
+    .resize(Number(width), Number(height))
+    .toFormat('jpg')
+    .toFile(
+      `./public/assets/images/thumb/${filename}-thumb(${width}x${height}).jpg`,
+      (err) => {
+        if (err) {
+          console.log(err);
+        }
+
+        res
+          .status(200)
+          .sendFile(thumbPath(`${filename}-thumb(${width}x${height}).jpg`));
+      }
+    );
+};
+
+export { imageController };
